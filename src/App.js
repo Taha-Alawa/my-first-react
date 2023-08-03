525564b514421fe8987bb8da0c198a96de44b869
Taha-Alawa <Tahaalawa366@gamail.com>
2023-08-03 17:39:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="titleT">i will learn react</h1>
+      <p>alalal</p>
+      <div>
+        <h2>sasfawshfkjlahf</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
